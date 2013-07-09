cefe6540e5e3be2897e2bbaa25c55f1284504e9b
Jan Tojnar <jtojnar@gmail.com>
2013-07-09 21:11:06 +0200

    shortened syntax using db query wrapper

--- web.js
+++ web.js
@@ -23,15 +23,9 @@ passport.deserializeUser(function(id, done) {
 	pg.connect(_dbUri, function(err, client) {
 		if(err) {return done(err, false);}
 
-		var query = client.query('select * from "user" where "id"=$1', [id]);
-		var rows = [];
-		query.on('row', function(row) {
-			rows.push(row);
-		});
-
-		query.on('end', function(result) {
+		dabin(client, 'select * from "user" where "id"=$1', [id], function(result) {
 			if(result.rowCount == 0) {return done(err, false);}
-			var user = rows[0];
+			var user = result.rows[0];
 			return done(err, user);
 		});
 	});
@@ -45,15 +39,9 @@ passport.use(new LocalStrategy(
 			pg.connect(_dbUri, function(err, client) {
 				if(err) {return done(err);}
 
-				var query = client.query('select * from "user" where "email"=$1', [email]);
-				var rows = [];
-				query.on('row', function(row) {
-					rows.push(row);
-				});
-
-				query.on('end', function(result) {
+				dabin(client, 'select * from "user" where "email"=$1', [email], function(result) {
 					if(result.rowCount == 0) {return done(null, false, {message: 'Unknown user ' + email});}
-					var user = rows[0];
+					var user = result.rows[0];
 					if(user.password != sha1(password+user.salt)) {return done(null, false, {message: 'Invalid password'});}
 					return done(null, user);
 				});
@@ -162,7 +150,7 @@ hbs.registerHelper('each', function(context, options) {
 
 hbs.registerPartials(path.join(__dirname, 'view', 'partial'));
 
-var pg = require('pg');
+var pg = require('pg'), dabin = require('./dabin').dabin;
 var moment = require('moment');
 
 var publicDir = path.join(__dirname, 'public');
@@ -196,21 +184,10 @@ app.configure('development', function() {
 
 app.get('/', function(req, res) {
 	pg.connect(_dbUri, function(err, client) {
-		var query = client.query('select * from update order by timestamp desc limit 10');
-		var updates = [];
-		query.on('row', function(row) {
-			updates.push(row);
-		});
-
-		query.on('end', function(result) {
-			var query = client.query('select * from event order by start asc');
-			var events = [];
-			query.on('row', function(row) {
-				events.push(row);
-			});
-
-			query.on('end', function(result) {
-				res.render('index', {updates: updates, events: events, identity: req.user});
+		dabin(client, 'select * from update order by timestamp desc limit 10', function(result) {
+			var updates = result.rows;
+			dabin(client, 'select * from event order by start asc', function(result) {
+				res.render('index', {updates: updates, events: result.rows, identity: req.user});
 			});
 		});
 	});
@@ -223,18 +200,35 @@ app.get('/events', function(req, res) {
 app.get('/results', function(req, res) {
 	var event = {id: 3, name: 'WRC 1998'};
 	pg.connect(_dbUri, function(err, client) {
-		var query = client.query('select * from member where event_id=$1', [event.id]);
-		var members = [];
-		query.on('row', function(row) {
-			members.push(row);
-		});
-
-		query.on('end', function(result) {
-			var query = client.query('select * from team where event_id=$1', [event.id]);
-			var teams = [];
+		dabin(client, 'select * from member where event_id=$1', [event.id], function(result) {
+			var members = result.rows;
+			var query = dabin(client, 'select * from team where event_id=$1', [event.id], function(result) {
+				res.render('results', {
+					title: 'Results of ' + event.name,
+					event: event,
+					teams: result.rows,
+					mo: {teams: [
+						{score: 400, time: '20:00:00', members: [{firstname: 'Petr', lastname: 'Novak', country_code: 'CZE'}]},
+						{score: 300, time: '20:00:00', members: [{firstname: 'Michal', lastname: 'Dvorak', country_code: 'CZE'}]},
+						{score: 200, time: '20:00:00', members: [{firstname: 'Jan', lastname: 'Novotny', country_code: 'CZE'}]}
+					]},
+					xo: {teams: [
+						{score: 340, time: '20:00:00', members: [{firstname: 'Mixr', lastname: 'Novak', country_code: 'CZE'}]},
+						{score: 310, time: '20:00:00', members: [{firstname: 'Mixal', lastname: 'Dvorak', country_code: 'CZE'}]},
+						{score: 300, time: '20:00:00', members: [{firstname: 'Mix', lastname: 'Novotny', country_code: 'CZE'}]}
+					]},
+					wo: {teams: [
+						{score: 200, time: '20:00:00', members: [{firstname: 'Petra', lastname: 'Novak', country_code: 'CZE'}]},
+						{score: 100, time: '19:00:00', members: [{firstname: 'Michala', lastname: 'Dvorak', country_code: 'CZE'}]},
+						{score: 100, time: '20:00:00', members: [{firstname: 'Jana', lastname: 'Novotny', country_code: 'CZE'}]}
+					]},
+					identity: req.user,
+					isMO: counters.MO, isXO: counters.XO, isWO: counters.WO, isMV: counters.MV, isXV: counters.XV, isWV: counters.WV, isMSV: counters.MSV, isXSV: counters.XSV, isWSV: counters.WSV, isMUV: counters.MUV, isXUV: counters.XUV, isWUV: counters.WUV, isMJ: counters.MJ, isXJ: counters.XJ, isWJ: counters.WJ, isM20: counters.M20, isX20: counters.X20, isW20: counters.W20, isM23: counters.M23, isX23: counters.X23, isW23: counters.W23
+				});
+			});
 			var counters = {MO: 0, XO: 0, WO: 0, MV: 0, XV: 0, WV: 0, MSV: 0, XSV: 0, WSV: 0, MUV: 0, XUV: 0, WUV: 0, MJ: 0, XJ: 0, WJ: 0, M20: 0, X20: 0, W20: 0, M23: 0, X23: 0, W23: 0};
 
-			query.on('row', function(row) {
+			query.on('row', function(row, result) {
 				row.members = [];
 				var mcache = [];
 				for(var i = 0; i < members.length; i++) {
@@ -247,7 +241,6 @@ app.get('/results', function(req, res) {
 					members.splice(mcache[i], 1);
 				};
 				mcache = [];
-				teams.push(row);
 				if(row.duration == 24) {
 					row.duration = '';
 					var gender = hbs.handlebars.helpers.genderclass(row.gender);
@@ -274,31 +267,6 @@ app.get('/results', function(req, res) {
 						row[gender+'20'] = ++counters[gender+'20'];
 					}
 				}
-			});
-
-			query.on('end', function(result) {
-				res.render('results', {
-					title: 'Results of ' + event.name,
-					event: event,
-					teams: teams,
-					mo: {teams: [
-						{score: 400, time: '20:00:00', members: [{firstname: 'Petr', lastname: 'Novak', country_code: 'CZE'}]},
-						{score: 300, time: '20:00:00', members: [{firstname: 'Michal', lastname: 'Dvorak', country_code: 'CZE'}]},
-						{score: 200, time: '20:00:00', members: [{firstname: 'Jan', lastname: 'Novotny', country_code: 'CZE'}]}
-					]},
-					xo: {teams: [
-						{score: 340, time: '20:00:00', members: [{firstname: 'Mixr', lastname: 'Novak', country_code: 'CZE'}]},
-						{score: 310, time: '20:00:00', members: [{firstname: 'Mixal', lastname: 'Dvorak', country_code: 'CZE'}]},
-						{score: 300, time: '20:00:00', members: [{firstname: 'Mix', lastname: 'Novotny', country_code: 'CZE'}]}
-					]},
-					wo: {teams: [
-						{score: 200, time: '20:00:00', members: [{firstname: 'Petra', lastname: 'Novak', country_code: 'CZE'}]},
-						{score: 100, time: '19:00:00', members: [{firstname: 'Michala', lastname: 'Dvorak', country_code: 'CZE'}]},
-						{score: 100, time: '20:00:00', members: [{firstname: 'Jana', lastname: 'Novotny', country_code: 'CZE'}]}
-					]},
-					identity: req.user,
-					isMO: counters.MO, isXO: counters.XO, isWO: counters.WO, isMV: counters.MV, isXV: counters.XV, isWV: counters.WV, isMSV: counters.MSV, isXSV: counters.XSV, isWSV: counters.WSV, isMUV: counters.MUV, isXUV: counters.XUV, isWUV: counters.WUV, isMJ: counters.MJ, isXJ: counters.XJ, isWJ: counters.WJ, isM20: counters.M20, isX20: counters.X20, isW20: counters.W20, isM23: counters.M23, isX23: counters.X23, isW23: counters.W23
-				});
 			});
 		});
 	});
