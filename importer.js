3a6b8e4a6b2318a8bb78f601771dbec441c7e053
Jan Tojnar <jtojnar@gmail.com>
2013-07-21 02:07:26 +0200

    Fixed importer

--- importer.js
+++ importer.js
@@ -9,31 +9,48 @@ var moment = require('moment');
 var parseString = require('xml2js').parseString;
 
 var event = { id: 11 };
+var teamI = 0;
+var memberI = 0;
+
+function recursive(teams, client, done) {
+	var team = teams[teamI].$;
+	team.event_id = event.id;
+	team.duration = team.duration ? team.duration : 24;
+	var members = teams[teamI].member;
+	client.query('insert into team(id, event_id, score, time, penalty, duration, gender, age, status) values($1, $2, $3, $4, $5, $6, $7, $8, $9)', [team.id, team.event_id, team.score, team.time, team.penalty, team.duration, team.gender, team.age, team.status || 'finished'], function() {
+		memberI = 0;
+		if(teamI < teams.length) {
+			return mrecursive(teams, client, done, team, members);
+		}
+	});
+}
+function mrecursive(teams, client, done, team, members) {
+	var member = members[memberI].$;
+	member.team_id = team.id;
+	member.event_id = event.id;
+	client.query('insert into member(team_id, event_id, firstname, lastname, country_code) values($1, $2, $3, $4, $5)', [member.team_id, member.event_id, member.firstname, member.lastname, member.country], function() {
+		if(memberI < members.length-1) {
+			memberI++;
+			return mrecursive(teams, client, done, team, members);
+		} else if(teamI < teams.length-1) {
+			teamI++;
+			memberI = 0;
+			return recursive(teams, client, done, team, members);
+		} else {
+			client.query('commit');
+			done();
+			return;
+		}
+	});
+}
 
 pg.connect(_dbUri, function(err, client, done) {
-	if(err) { return console.log(err); }
 	client.query('begin');
 
 	fs.readFile(__dirname + '/resultsbroumy.xml', function(err, data) {
-		parseString(data, function (err, results) {
+		parseString(data, function(err, results) {
 			var teams = results.results.team;
-			for(var i = 0, j = teams.length; i<j; i++) {
-				var team = teams[i].$;
-				team.event_id = event.id;
-				team.duration = team.duration ? team.duration : 24;
-
-				var query = client.query('insert into team(id, event_id, score, time, penalty, duration, gender, age, status) values($1, $2, $3, $4, $5, $6, $7, $8, $9)', [team.id, team.event_id, team.score, team.time, team.penalty, team.duration, team.gender, team.age, team.status || 'finished']);
-
-				var members = teams[i].member;
-				for(var k = 0, l = members.length; k<l; k++) {
-					var member = members[k].$;
-					member.team_id = team.id;
-					member.event_id = event.id;
-					var query = client.query('insert into member(team_id, event_id, firstname, lastname, country_code) values($1, $2, $3, $4, $5)', [member.team_id, member.event_id, member.firstname, member.lastname, member.country]);
-				}
-			}
-			client.query('commit');
-			done();
+			return recursive(teams, client, done);
 		});
 	});
 });
