928ee37aa7dfd561d1ef9a5d153839c60fe0b353
Jan Tojnar <jtojnar@gmail.com>
2026-06-12 08:51:45 +0200

    Remove JavaScript backend
    We now have a static site generator written in Rust.

--- db.js
+++
@@ -1,8 +0,0 @@
-import pg from 'pg';
-const { Pool } = pg;
-
-export const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-});
-
-export const query = (text, params) => pool.query(text, params);
--- route/updates.js
+++
@@ -1,7 +0,0 @@
-import * as db from '../db.js';
-import sql from "sql-template-tag";
-
-export async function archive(req, res) {
-	let data = await db.query(sql`select * from update order by timestamp desc`);
-	res.render('updates_archive', {updates: data.rows, identity: req.user, first: true, last: true});
-};
